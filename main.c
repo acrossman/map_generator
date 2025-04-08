28cc9fb5440d23a543508736e13a87453a2c3b52
Crow Crossman <115806315+acrossman@users.noreply.github.com>
2025-04-08 12:16:34 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*	main.c
+ *
+ *	Program generates a map with pre-defined parameters and prints it to console.
+ *	written by Crow
+ *
+ *	version 1.0
+ */
+
 #include <stdio.h>
 #include "map_gen.h"
 
