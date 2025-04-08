576eccb13fa6655c210ad15df9d14860c4a3d301
Crow Crossman <115806315+acrossman@users.noreply.github.com>
2025-04-08 12:46:37 -0700

    Update map_gen.h

--- map_gen.h
+++ map_gen.h
@@ -4,13 +4,13 @@
 #define COL 31
 #define ROW 31
 
-#define water "\033[1;34m ~ \033[0m"
-#define forest "\033[1;32m T \033[0m"
-#define mountain "\033[1;31m ^ \033[0m"
-#define thicket "\033[38;2;0;100;0m t \033[0m"
-#define oats "\033[38;5;230m o \033[0m"
-#define grass "\033[38;2;139;69;19m w \033[0m"
-#define wall "\033[5;37m{ }\033[0m"
+#define water    "\033[30;44m ~ \033[0m"
+#define forest   "\033[30;42m T \033[0m"
+#define mountain "\033[30;41m ^ \033[0m"
+#define thicket  "\033[30;48;2;0;100;0m t \033[0m"
+#define oats     "\033[30;48;5;230m o \033[0m"
+#define grass    "\033[30;48;2;139;69;19m w \033[0m"
+#define wall     "\033[5;37m{ }\033[0m"
 
 // Structs
 typedef struct tile {
